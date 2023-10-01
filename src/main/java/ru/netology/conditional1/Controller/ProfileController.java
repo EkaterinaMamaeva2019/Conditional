b71b93eeda2f3bdf7d77b8887a0253d600e3e4b6
Ekaterina Mamaeva <katerina.grozdova@mail.ru>
2023-10-02 00:53:41 +0300

    TestContainers

--- src/main/java/ru/netology/conditional1/Controller/ProfileController.java
+++ src/main/java/ru/netology/conditional1/Controller/ProfileController.java
@@ -8,7 +8,7 @@ import ru.netology.conditional1.SystemProfile.SystemProfile;
 @RestController
 @RequestMapping("/")
 public class ProfileController {
-    private SystemProfile profile;
+    private final SystemProfile profile;
 
     public ProfileController(SystemProfile profile) {
         this.profile = profile;
